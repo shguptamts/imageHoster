95e46374c9a90a9359d769979cf3eddeec8beb53
Shubham Gupta <shgupta@vmware.com>
2020-09-14 02:28:08 +0530

    Only the owner of the image can edit the image
    - cause: no authorization of current owner on the image
    - fix:  added pre-check of ownership  on edit functionality

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -92,13 +92,27 @@ public class ImageController {
     //The method first needs to convert the list of all the tags to a string containing all the tags separated by a comma and then add this string in a Model type object
     //This string is then displayed by 'edit.html' file as previous tags of an image
     @RequestMapping(value = "/editImage")
-    public String editImage(@RequestParam("imageId") Integer imageId, Model model) {
+    public String editImage(@RequestParam("imageId") Integer imageId, Model model, HttpSession session) {
         Image image = imageService.getImage(imageId);
 
-        String tags = convertTagsToString(image.getTags());
+        //get current logged in user
+        User loggedInUser = (User) session.getAttribute("loggeduser");
+
+        //add image in the model
         model.addAttribute("image", image);
-        model.addAttribute("tags", tags);
-        return "images/edit";
+
+        //check if the current user is the owner of image
+        //if yes, then direct the user to edit page, also add existing tags text in  model
+        //if no, the direct user to images/image page (same page), also add editError key and existing tags list in model
+        if( loggedInUser.getId() == image.getUser().getId()){
+            String tags = convertTagsToString(image.getTags());
+            model.addAttribute("tags", tags);
+            return "images/edit";
+        }else{
+            model.addAttribute("editError", true);
+            model.addAttribute("tags", image.getTags());
+            return "images/image";
+        }
     }
 
     //This controller method is called when the request pattern is of type 'images/edit' and also the incoming request is of PUT type
